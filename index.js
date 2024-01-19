2519b596d4053b08bf49d90fecb0ab2dbd4fe44d
Shawki45 <shawkihaifa@yahoo.com>
2024-01-19 10:37:03 -0500

    added some code

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown')
+
 // TODO: Create an array of questions for user input
 const questions = [{}];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    generateMarkdown(data)
+}
+
 
 // TODO: Create a function to initialize app
 async function init() {
