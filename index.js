d7d203d90cc9ec982b519a5a9831d0753af3188a
Shawki45 <shawkihaifa@yahoo.com>
2024-01-19 10:32:19 -0500

    added remainder of the files

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{}];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+ const answers = await inquirer.prompt(questions);
+ // pass this stuff to the output function
+ writeToFile('readme.md', answers)
+}
 
 // Function call to initialize app
 init();
