11b25c7b2c2952df241674c903209ce2b92bd180
Shawki45 <shawkihaifa@yahoo.com>
2024-01-19 15:24:51 -0500

    added the license

--- develop/index.js
+++ develop/index.js
@@ -3,13 +3,23 @@ const inquirer = require('inquirer')
 const generateMarkdown = require('../utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [{}];
+const questions = [{
+    type:"input",
+    name:"title",
+    message:"what is your project name? "
+}];
 
-// TODO: Create a function to write README file
+// write README file
 function writeToFile(fileName, data) {
-    generateMarkdown(data)
-}
+    // markdown
+ const contents = generateMarkdown(data)
+ // write file 
+ fs.writeToFile(filename, contents, (err)=> {
+   err? new Error("Write file blew up"): console.log("Normal Error");
 
+ })
+
+}
 
 // TODO: Create a function to initialize app
 async function init() {
